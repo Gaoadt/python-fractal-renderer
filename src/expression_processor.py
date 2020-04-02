ca4d9337ab5a58909b9b1ceced1f8898733dc12d
Gregory Minakov <gaoadt@outlook.com>
2020-04-03 00:41:39 +0300

    Basic interface and expression parser has been created

---
+++ src/expression_processor.py
@@ -0,0 +1,299 @@
+from abc import abstractclassmethod
+from copy import copy, deepcopy
+import sys
+class IExpressionProcessor:
+    @abstractclassmethod
+    def getParsedExpression(self, expressionString):
+        pass
+
+class IExpression:
+    @abstractclassmethod
+    def getNiceString(self):
+        pass
+
+class ExpressionOperator:
+    def __init__(self, symbolString, valence = 1):
+        self.symbolString = symbolString
+        self.valence = valence
+
+class Expression:
+    symbol =''
+    
+    def setArgs(self, *args):
+        for i in range(self.valence):
+            self.args[i] = args[i]
+        self.needsSubLinking = False
+        return self
+
+    def print(self):
+        return f"{self.symbol}({','.join([x.print() for x in self.args])})"
+    
+    def __deepcopy__(self, memo={}):
+        new = self.__class__()
+        new.__dict__.update(self.__dict__)
+        new.args = deepcopy(self.args, memo)
+        return new
+
+class BinaryExpr(Expression):
+    valence = 2
+    args = [None, None]
+    priority = 0
+    needsSubLinking = False
+
+class UnaryExpr(Expression):
+    valence = 1
+    args = [None]
+    needsSubLinking = False
+    def setArg(self, value):
+        self.args[0] = value
+        return self
+
+class MultiplyExpr(BinaryExpr):
+    symbol = "*"
+    priority = 1
+
+class SumExpr(BinaryExpr):
+    symbol = "+"
+
+class SubtractExpr(BinaryExpr):
+    symbol = "-"
+
+class DivideExpr(BinaryExpr):
+    symbol = "/"
+    priority = 1
+
+
+class ConstantExpr(Expression):
+    valence = 0
+    args = []
+    needsSubLinking = False
+    def __init__(self, value):
+        self.value = value
+    def print(self):
+        return str(self.value)
+
+class NamedVarExpr(Expression):
+    valence = 0
+    needsSubLinking = False
+    args = []
+    def __init__(self, identifierName):
+        self.identifierName = identifierName
+    def print(self):
+        return self.identifierName
+
+class UnaryMinus(UnaryExpr):
+    symbol = "-"
+
+class UnaryPlus(UnaryExpr):
+    symbol = '+'
+
+
+
+class InvalidExpressionException(Exception):
+    def __init(self, message):
+        super.__init__(message)
+
+class EmptyExpressionException(InvalidExpressionException):
+    def __init__(self, index):
+        super(EmptyExpressionException,self).__init__(f"Expected non-empty expression here at {index}")
+
+class UnfinishedExpressionException(InvalidExpressionException):
+    def __init__(self, index):
+        super(UnfinishedExpressionException,self).__init__(f"Expected expression at {index}")
+
+class InvalidConstantException(InvalidExpressionException):
+    def __init__(self, index):
+        super(InvalidConstantException,self).__init__(f"Expected valid constant at {index}")
+
+class InvalidBracketsException(InvalidExpressionException):
+    def __init__(self, index):
+        super(InvalidBracketsException,self).__init__(f"Expected closing bracket ar {index}")
+
+class DefaultExpressionProcessor(IExpressionProcessor):
+    
+    unaryOperations = [UnaryPlus(), UnaryMinus()]
+    binaryOperations = [SumExpr(), DivideExpr(), MultiplyExpr(), SubtractExpr()]
+
+    def __currentCharacter(self):
+        return self.__expressionWithoutSpaces[self.__index]
+
+
+    def __cutUnary(self):
+        for operation in self.unaryOperations:
+            if self.__currentCharacter() == operation.symbol:
+                self.__index += 1
+                return deepcopy(operation).setArg(self.__getInfixExpression())
+        return None
+    
+    def __isFloatConstantSymbol(self):
+        return (not self.__isOver()) and self. __currentCharacter() in "0123456789."
+    
+    def __isIdentifierSymbol(self):
+        return (not self.__isOver()) and self.__currentCharacter() in "1234567890abcdefghijklmnopqrstuvwzxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    def __cutBrackets(self):
+        if self.__currentCharacter() == '(':
+            self.__index += 1
+            expr =  self.__getValidExpression()
+            if self.__isOver():
+                raise InvalidBracketsException(self.__index)
+            if self.__currentCharacter() != ')':
+                raise InvalidBracketsException(self.__index)
+            self.__index += 1
+            return expr
+        return None
+
+    def __cutFloatConstant(self):
+        if not self.__isFloatConstantSymbol():
+            return None
+        
+        strConstant = ""
+        while self.__isFloatConstantSymbol():
+            strConstant += self.__currentCharacter()
+            self.__index += 1
+
+        try:
+            floatConstant = float(strConstant)
+            return ConstantExpr(floatConstant)
+
+        except ValueError:
+            raise InvalidConstantException(self.__index)
+
+    def __isOver(self):
+        return self.__index >= self.__expLen
+    
+
+    def __isSubexprEnd(self):
+        return self.__isOver() or self.__currentCharacter() == ')'
+    
+    def __isBinaryOperatorNext(self):
+        for operation in self.binaryOperations:
+            if operation.symbol == self.__currentCharacter():
+                return True
+        return False
+    
+    def __isNextBracketInfixExpr(self):      
+        if not self.__isOver():
+            return self.__currentCharacter() == '('
+        return False
+
+
+    def __getInfixExpression(self):
+        if(self.__isSubexprEnd()):
+            raise UnfinishedExpressionException(self.__index)
+        
+        bracketsCutResult = self.__cutBrackets()
+        if(bracketsCutResult != None):
+            if self.__isNextBracketInfixExpr():
+                return deepcopy(MultiplyExpr()).setArgs(bracketsCutResult, self.__getInfixExpression())
+            
+            if not self.__isOver():
+                if not self.__isBinaryOperatorNext():
+                    return deepcopy(MultiplyExpr()).setArgs(bracketsCutResult, self.__getInfixExpression())
+            return bracketsCutResult
+
+        unaryCutResult = self.__cutUnary()
+        if(unaryCutResult != None):
+            return unaryCutResult
+        
+        prefixConstant = self.__cutFloatConstant()
+        
+        identifier = ""
+        while(self.__isIdentifierSymbol()):
+            identifier += self.__currentCharacter()
+            self.__index += 1
+        
+        
+
+
+        varConstant = None
+        if identifier != "":
+            varConstant = NamedVarExpr(identifier)
+        
+        if(self.__isNextBracketInfixExpr()):
+            if(varConstant == None):
+                varConstant = self.__getInfixExpression()
+            else:
+                varConstant = deepcopy(MultiplyExpr()).setArgs(varConstant, self.__getInfixExpression())
+
+        if prefixConstant == None:
+            if varConstant == None:
+                raise UnfinishedExpressionException(self.__index)
+            else:
+                return varConstant
+        else:
+            if varConstant == None:
+                return prefixConstant
+            else:
+                return deepcopy(MultiplyExpr()).setArgs(prefixConstant, varConstant)
+        
+
+    def __getPairingSymbol(self):
+        for operation in self.binaryOperations:
+            if operation.symbol == self.__currentCharacter():
+                self.__index += 1
+                resOper = deepcopy(operation)
+                resOper.needsSubLinking = True
+                return resOper
+        raise UnfinishedExpressionException(self.__index)
+    
+    def __subLinkList(self, subResult):
+        if(self.__subListIndex >= len(subResult)):
+            raise UnfinishedExpressionException(self.__index)
+        currentExpression = subResult[self.__subListIndex]
+        self.__subListIndex += 1
+        if currentExpression.needsSubLinking:
+            for i in range(currentExpression.valence):
+                currentExpression.args[currentExpression.valence - 1 - i] = self.__subLinkList(subResult)
+            currentExpression.needsSubLinking = False
+        return currentExpression
+
+    def __getValidExpression(self):
+        if self.__isSubexprEnd():
+            raise EmptyExpressionException(self.__index)
+
+        subResult = []
+        subStack = []
+        while(not self.__isSubexprEnd()):
+            subResult.append(self.__getInfixExpression())
+            if(self.__isSubexprEnd()):
+                break
+            pairingSymbol = self.__getPairingSymbol()
+            
+            while len(subStack) != 0:
+                if subStack[-1].priority < pairingSymbol.priority:
+                    break
+                subResult.append(subStack.pop())
+               
+            subStack.append(pairingSymbol)
+        
+        while(len(subStack) != 0):
+            subResult.append(subStack.pop())
+
+        if(len(subResult) % 2 == 0):
+            raise UnfinishedExpressionException(self.__index)
+        
+        self.__subListIndex = 0
+        return self.__subLinkList(subResult[::-1])
+        
+
+            
+
+
+    def getParsedExpression(self, expressionString):
+        self.__expressionWithoutSpaces = ''.join(expressionString.split(' '))
+        self.__index = 0
+        self.__expLen = len(self.__expressionWithoutSpaces)
+        return self.__getValidExpression()
+    
+    def isParseable(self, string):
+        try:
+            self.getParsedExpression(string)
+            return True
+        except InvalidExpressionException:
+            return False
+
+
+#string = input()
+#processor = DefaultExpressionProcessor()
+#print(processor.getParsedExpression(string).print())
---
+++ src/main_window.py
@@ -0,0 +1,79 @@
+import tkinter as tk
+from expression_processor import DefaultExpressionProcessor
+
+proc = DefaultExpressionProcessor()
+
+class MainWindow:
+
+    def __iterations_changed_callback(self, event):
+        iterationsStr = self.iterations_string_var.get()
+        try:
+            int(iterationsStr)
+            self.iterations_entry.config(foreground = 'black')
+            
+        except ValueError:
+            self.iterations_entry.config(foreground = 'tomato')
+    def __radius_changed_callback(self, event):
+        radiusStr = self.radius_string_var.get()
+        try:
+            float(radiusStr)
+            self.radius_entry.config(foreground = 'black')
+            
+        except ValueError:
+            self.radius_entry.config(foreground = 'tomato')
+    def __formula_changed_callback(self, event):
+        formulaStr = self.formula_string_var.get()
+        if(proc.isParseable(formulaStr)):
+            self.formula_entry.config(foreground = 'black')
+        else:
+            self.formula_entry.config(foreground = 'tomato')        
+
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Python Fractal Renderer by Gaoadt")
+        self.main_frame = tk.Frame(self.root)
+        
+        self.main_label = tk.Label(self.main_frame, text = "Enter formula and parameters")
+        self.main_label.grid(columnspan = 2)
+        
+        self.iterations_label = tk.Label(self.main_frame, text =  "Number of iterations:")
+        self.iterations_label.grid(row = 1, column = 0)
+        self.iterations_string_var = tk.StringVar()
+        self.iterations_string_var.set(100)
+        self.iterations_entry = tk.Entry(self.main_frame, textvariable = self.iterations_string_var)
+        self.iterations_entry.grid(row = 1, column = 1)
+        self.iterations_entry.bind("<FocusOut>", self.__iterations_changed_callback)
+
+
+        self.radius_label = tk.Label(self.main_frame, text =  "Radius of divergence:")
+        self.radius_label.grid(row = 2, column = 0)
+        self.radius_string_var = tk.StringVar()
+        self.radius_string_var.set(2.0)
+        self.radius_entry = tk.Entry(self.main_frame, textvariable = self.radius_string_var)
+        self.radius_entry.grid(row = 2, column = 1)
+        self.radius_entry.bind("<FocusOut>", self.__radius_changed_callback)
+        
+
+        self.formula_label = tk.Label(self.main_frame, text =  "Iteration formula: x'(x) = ")
+        self.formula_label.grid(row = 3, column = 0)
+        self.formula_string_var = tk.StringVar()
+        self.formula_string_var.set("x * x + pos")
+        self.formula_entry = tk.Entry(self.main_frame, textvariable = self.formula_string_var)
+        self.formula_entry.grid(row = 3, column = 1)
+        self.radius_entry.bind("<FocusOut>", self.__formula_changed_callback)
+        self.radius_entry.bind("<FocusIn>", self.__formula_changed_callback)
+
+        self.name_label = tk.Label(self.main_frame, text =  "Fractal's name")
+        self.name_label.grid(row = 4, column = 0)
+        self.name_string_var = tk.StringVar()
+        self.name_string_var.set("Some fractal")
+        self.name_entry = tk.Entry(self.main_frame, textvariable = self.name_string_var)
+        self.name_entry.grid(row = 4, column = 1)
+
+
+        self.render_button = tk.Button(self.main_frame, text = "Render")
+        self.render_button.grid(row = 5, columnspan = 2)
+
+        self.main_frame.pack()
+        self.root.mainloop()
+MainWindow()
