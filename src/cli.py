b7cf04647770e697c5774a0dbd8bd77e958389ff
Gregorii Miankov <gaoadt@outlook.com>
2020-04-24 01:09:48 +0300

    Scracth for cli support

---
+++ src/cli.py
@@ -0,0 +1,3 @@
+def launch(args):
+    print("Unexpected CLI Args")
+    print(args)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from importlib.util import find_spec
-from sys import exit
+import sys
 
 tkinter_install_text = """
 Cannot find tkinter module
@@ -8,10 +8,13 @@ Please install tkinter https://tkdocs.com/tutorial/install.html
 
 if __name__ == '__main__':
     tkinter = find_spec("tkinter")
-
     if tkinter is None:
         print(tkinter_install_text)
-        exit(0)
-
-    from main_window import MainWindow
-    MainWindow()
+        sys.exit(0)
+    
+    if len(sys.argv) > 1:
+        import cli
+        cli.launch(sys.argv[1:])
+    else:
+        from main_window import MainWindow
+        MainWindow()
